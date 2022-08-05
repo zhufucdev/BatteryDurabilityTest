bd6ced95d9063c12edc6fbd9ef7329f93e466523
zhufucdev <1095291434@qq.com>
2022-08-05 10:37:06 +0800

    Production ready

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from common import *
 
-MS_WORK_DURATION = 5
+MS_WORK_DURATION = 600
 DEFAULT_BROWSING_TIME = 30
-TOTAL_BROWSING_TIME = 5
+TOTAL_BROWSING_TIME = 600
 BROWSE_PAGES = ["jd.com", "taobao.com", "sina.com.cn", "163.com", "sohu.com", ("ithome.com", 20), ("chiphell.com", 10),
                 ("bbs.nga.cn", 10), ("gamersky.com", 15), ("3dmgame.com", 20), ("4399.com", 23),
                 ("https://www.apple.com.cn/macbook-air-m2/", 80)]
-VIDEO_WATCH_TIME = 5
-QQ_CHAT_TIME = 5
+VIDEO_WATCH_TIME = 600
+QQ_CHAT_TIME = 600
 
 
 class TestInitialization(Test):
@@ -52,29 +52,29 @@ class StandardTest(Test):
             ),
             Pause(0.5),
             HitEscapeKey(),
-            #
-            # ppt_context,
-            # Pause(1),
-            # TimerLoop(
-            #     "ppt_slideshow",
-            #     [
-            #         PPTNext(ppt_context),
-            #         ppt_context.next_slide()
-            #     ],
-            #     MS_WORK_DURATION
-            # ),
-            # Pause(0.5),
-            # HitEscapeKey(),
-            #
-            # word_context,
-            # Pause(1),
-            # TimerLoop("word_typewrite", [WordTypeNonsense()], MS_WORK_DURATION),
-            #
-            # Pause(1),
-            # TimerLoop("web_switch_page", web_browsing, TOTAL_BROWSING_TIME, should_not_stop_when=['quit', 'pause']),
-            # OpenAndBrowse("https://www.bilibili.com/video/BV1Af4y1f7NJ", 0),
-            # Pause(VIDEO_WATCH_TIME),
-            # Quit(),
+
+            ppt_context,
+            Pause(1),
+            TimerLoop(
+                "ppt_slideshow",
+                [
+                    PPTNext(ppt_context),
+                    ppt_context.next_slide()
+                ],
+                MS_WORK_DURATION
+            ),
+            Pause(0.5),
+            HitEscapeKey(),
+
+            word_context,
+            Pause(1),
+            TimerLoop("word_typewrite", [WordTypeNonsense()], MS_WORK_DURATION),
+
+            Pause(1),
+            TimerLoop("web_switch_page", web_browsing, TOTAL_BROWSING_TIME, should_not_stop_when=['quit', 'pause']),
+            OpenAndBrowse("https://www.bilibili.com/video/BV1Af4y1f7NJ", 0),
+            Pause(VIDEO_WATCH_TIME),
+            Quit(),
 
             WakeQQ(),
             WaitUntilCPUFree(),
